e57b8cd4f8d451c95c0084114540b717d269e3f6
Asrst <addasaiteja@gmail.com>
2020-10-10 19:25:25 +0530

    add virtualenv

--- qrcode_py/packages/default/pyqr/__main__.py
+++ qrcode_py/packages/default/pyqr/__main__.py
@@ -1,9 +1,11 @@
 
 def main(args):
-      import pyqrcode
-      text = args.get("text", "need generate QR code from texts ?, visit our qrcode app")
-      code = pyqrcode.create(text)
-      image_as_str = code.png_as_base64_str(scale=5)
-      html_img = '<img src="data:image/png;base64,{}">'.format(image_as_str)
-      html_img = '<img src=>'
-      return {"headers": {'content-type': 'text/html; charset=UTF-8'}, "body": html_img}
+      try:
+            import pyqrcode
+            text = args.get("text", "need generate QR code from texts ?, visit our qrcode app")
+            code = pyqrcode.create(text)
+            image_as_str = code.png_as_base64_str(scale=5)
+            html_img = '<img src="data:image/png;base64,{}">'.format(image_as_str)
+            return {"headers": {'content-type': 'text/html; charset=UTF-8'}, "body": html_img}
+      except Exception as e:
+            return {'error': str(e)}
